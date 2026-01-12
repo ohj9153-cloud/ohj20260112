b482ef6eb7f8c59fb5bbbd4212c5884c6068bd24
ohj9153-cloud <ohj9153@gmail.com>
2026-01-12 11:02:15 +0000

    하트추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈ohj의 앱")
+st.title("🎈ohj의 앱♥")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
